670b6bb3b12f0f97bc00c1b0884c4d02ab709e8d
luobin <402661663@qq.com>
2026-09-22 17:04:00 +0800

    style(back-top): change transition to slide-up

--- src/components/common/BackTop.vue
+++ src/components/common/BackTop.vue
@@ -6,10 +6,10 @@
 </script>
 
 <template>
-  <Transition name="slide-left">
+  <Transition name="slide-up">
     <button
       v-show="visible"
-      class="bg-primary hover:bg-primary/90 focus:ring-primary/50 fixed right-16 bottom-24 z-40 flex h-10 w-10 cursor-pointer items-center justify-center rounded-full text-white shadow-lg transition-all hover:shadow-xl focus:ring-2 focus:outline-none md:bottom-8"
+      class="bg-primary hover:bg-primary/90 focus:ring-primary/50 fixed right-16 bottom-20 z-40 flex h-10 w-10 cursor-pointer items-center justify-center rounded-full text-white shadow-lg transition-all hover:shadow-xl focus:ring-2 focus:outline-none max-sm:right-6 max-sm:bottom-6"
       aria-label="BackTop"
       @click="scrollToTop"
     >
